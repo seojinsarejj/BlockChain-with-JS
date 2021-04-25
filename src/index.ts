4851fa1536d7f6a080f6ef884e9d3f9a30d0bc5b
seojinsarejj <s19009@naver.com>
2021-04-25 14:21:53 +0900

    [ADD] Add checking validation add Adding New Block

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,13 @@ class Block {
         ) :string => 
             CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
+    static validateStructure = (aBlock: Block): boolean => 
+        typeof aBlock.index === "number" && 
+        typeof aBlock.hash === "string" && 
+        typeof aBlock.previousHash === "string" && 
+        typeof aBlock.timestamp === "number" && 
+        typeof aBlock.data === "string";
+
     public index: number;
     public hash: string;
     public previousHash: string;
@@ -65,7 +72,30 @@ const createNewBlock = (data: string): Block => {
         return newBlock;
 };
 
+
+
 console.log(createNewBlock("hello"), createNewBlock("bye bye"))
 
+const getHashforBlock = (aBlock: Block): string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+
+const isBlockValid = (candidateBlock :Block, previousBlock: Block): boolean => {
+    if (Block.validateStructure(candidateBlock)) {
+        return false;
+    } else if(previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    } else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+    if(isBlockValid(candidateBlock,getLatestBlock())) {
+        blockchain.push(candidateBlock);
+    }
+};
 
 export {};
